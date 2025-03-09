8312f374338277933973b138c43f2802555b4ede
Erfan <erfanalaei2001@gmail.com>
2025-03-09 21:19:17 +0330

    Adding routes to the app

--- src/server.ts
+++ src/server.ts
@@ -2,21 +2,32 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import helmet from 'helmet';
-import morgan from 'morgan';
+import passport from './config/passport';
+import { env } from './config/env';
+import createRouter from './routes';
+import errorMiddleware from './middlewares/error.middleware';
+import morgan from 'morgan'
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = env.PORT || 5000;
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', (req, res) => {
-  res.send('Chat App Backend is Running!');
-});
+
+// Passport
+app.use(passport.initialize());
+
+// Routes
+app.use('/api',createRouter)
+
+// Error handling middleware
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
