7c0bce3f7daba11f44195c09b54cca17dd1795d5
Erfan <erfanalaei2001@gmail.com>
2025-03-09 17:38:34 +0330

    More accurate user validation

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -6,13 +6,17 @@ import {
   getUserById, 
   updateUser, 
   deleteUser 
-} from '../services/user.service'
-import { createUserSchema,loginUserUserSchema,updateUserSchema } from '../validations/user.validation';
+} from '../services/user.service';
+import { createUserSchema, loginUserUserSchema, updateUserSchema } from '../validations/user.validation';
 
 export async function createUserController(req: Request, res: Response) {
+  const validation = createUserSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({ error: validation.error.format() });
+  }
+
   try {
-    const validatedData = createUserSchema.parse(req.body)
-    const user = await createUser(validatedData);
+    const user = await createUser(validation.data);
     res.status(201).json({ message: 'User created successfully.', user });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
@@ -20,9 +24,13 @@ export async function createUserController(req: Request, res: Response) {
 }
 
 export async function loginUserController(req: Request, res: Response) {
+  const validation = loginUserUserSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({ error: validation.error.format() });
+  }
+
   try {
-    const validatedData = loginUserUserSchema.parse(req.body)
-    const token = await loginUser(validatedData);
+    const token = await loginUser(validation.data);
     res.status(200).json({ message: 'Login was successful.', token });
   } catch (error: any) {
     res.status(401).json({ error: error.message });
@@ -48,9 +56,13 @@ export async function getUserByIdController(req: Request, res: Response) {
 }
 
 export async function updateUserController(req: Request, res: Response) {
+  const validation = updateUserSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({ error: validation.error.format() });
+  }
+
   try {
-    const validatedData = updateUserSchema.parse(req.body)
-    const user = await updateUser(req.params.id, validatedData);
+    const user = await updateUser(req.params.id, validation.data);
     res.status(200).json({ message: 'User updated.', user });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
